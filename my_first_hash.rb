59423cf806f935076c66a5281b1bf540089d1593
Ignacio R Espino <irespino@capozios.com>
2018-08-29 03:44:20 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,8 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  hash = {
+    "Name" => "Ignacio",
+    "Age" => 28,
+  }
 end
 
 
